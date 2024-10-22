0d0a3b95cc3c163a3b3cba4e54b231b984c2c819
Naufal Adna Garibaldi <rayhanaldi82@gmail.com>
2024-10-22 15:44:06 +0700

    attempt 1 - 14.15

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 // TODO: Implement the password generation logic based on user input
 
 const generatePassword = (length, options) => {
-    // Character sets for password generation
-    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowercase = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const specialChars = "!@#$%^&*()";
+  // Character sets for password generation
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const specialChars = "!@#$%^&*()";
 
-    // TODO: Create a variable for the character set based on selected options
+  // TODO: Create a variable for the character set based on selected options
+  let setCharacter = "";
 
-    // TODO: Generate the password based on the selected criteria
-    return password;
+  if (options.includeUppercase) setCharacter += uppercase;
+  if (options.includeLowercase) setCharacter += lowercase;
+  if (options.includeNumbers) setCharacter += numbers;
+  if (options.includeSpecialChars) setCharacter += specialChars;
+
+  // Make sure at least one character type is selected
+  if (setCharacter === "") {
+    return "pilih salah satu karakter!";
+  }
+
+  // TODO: Generate the password based on the selected criteria
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * setCharacter.length);
+    password += setCharacter[randomIndex];
+  }
+
+  return password;
 };
 
 // TODO: Add event listener to the button to call generatePassword and display the output
